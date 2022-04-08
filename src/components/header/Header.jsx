3ac5a8badb056ccfea141fb0d0f61ef1c02ee10c
Rutik Gagare <rutikgagare091971@gmail.com>
2022-04-08 16:39:27 +0530

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () =>{
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Rutik Gagare</h1>
+        <h1>Rutik Navnath Gagare</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA/>
         <HeaderSocial/>
